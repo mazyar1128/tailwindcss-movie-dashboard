af668a95a8dfb459cb8f47c622e496d5361c1e96
mazyar1128 <mazyar1128@yahoo.com>
2022-03-28 22:44:02 +0430

    First commit, dashboard template designed

---
+++ README.md
@@ -0,0 +1,24 @@
+
+# Tailwindcss Movie Dashboard Template
+
+A responsive movie dashboard template designed with tailwindcss.
+
+
+## Screenshots
+
+![App Screenshot](screenshot.jpg)
+
+
+## Installation & Development
+
+For furthur development, Tailwindcss and Node.js dependencies need to be installed by npm.
+
+```bash
+  npm install 
+```
+
+In development, Tailwind CLI will be needed to watch and build the style.css
+
+```bash
+  npm run build 
+```
---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: ["*.{html,js}"],
+  theme: {
+    screens:{
+      'xs': '350px',
+      ...defaultTheme.screens,
+    },
+    extend: {
+      colors: {
+       
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat','sans-serif'],
+        'poppins': ['Poppins','sans-serif'],
+       
+      },
+      fontSize: {
+        'xxs': '.65rem'
+      },
+      
+    }
+  },
+  plugins: [],
+}
